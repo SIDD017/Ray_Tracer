df8c150c3b61a41f8889b6ef63661ef6afcd906d
LOL <LOL2>
2020-03-09 12:03:37 +0530

    Added camera class, antialiasing and diffuse material(gamma corrected)

--- RayTracer_Test1.cpp
+++ RayTracer_Test1.cpp
@@ -2,14 +2,27 @@
 #include "float.h"
 #include "Hittable_list.h"
 #include "Sphere.h"
+#include "Camera.h"
+#include "Random.h"
 #include <iostream>
+
+Vec3 random_in_unit_sphere()
+{
+	Vec3 p;
+	do
+	{
+		p = 2.0 * Vec3(random_double(), random_double(), random_double()) - Vec3(1, 1, 1);
+	} while (p.squared_length() >= 1.0);
+	return p;
+}
 
 Vec3 color(const Ray& r, Hittable *world)
 {
 	hit_record rec;
-	if (world->hit(r, 0.0, FLT_MAX, rec))
+	if (world->hit(r, 0.001, FLT_MAX, rec))
 	{
-		return 0.5 * Vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+		Vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5 * color(Ray(rec.p,target - rec.p), world);
 	}
 	else {
 		Vec3 unit_direction = unit_vector(r.direction());
@@ -22,6 +35,7 @@ int main()
 {
 	int nx = 1280;
 	int ny = 720;
+	int ns = 10;
 	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 	Vec3 lower_left_corner(-2.0,-1.0,-1.0);
 	Vec3 horizontal(4.0, 0.0, 0.0);
@@ -32,17 +46,21 @@ int main()
 	list[0] = new Sphere(Vec3(0, 0, -1), 0.5);
 	list[1] = new Sphere(Vec3(0, -100.5, -1), 100);
 	Hittable* world = new Hittable_list(list, 2);
+	Camera cam;
 	for (int j = ny - 1; j >= 0; j--)
 	{
 		for (int i = 0; i < nx; i++)
 		{
-			float u = float(i) / float(nx);
-			float v = float(j) / float(ny);
-			Ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-			
-			Vec3 p = r.point_at_parameter(2.0);
-			Vec3 col = color(r, world);
-
+			Vec3 col(0, 0, 0);
+			for (int s = 0; s < ns; s++)
+			{
+				float u = float(i + random_double()) / float(nx);
+				float v = float(j + random_double()) / float(ny);
+				Ray r = cam.get_ray(u, v);
+				col += color(r, world);
+			}
+			col /= float(ns);
+			col = Vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 			int ir = int(255.99 * col[0]);
 			int ig = int(255.99 * col[1]);
 			int ib= int(255.99 * col[2]);
@@ -51,25 +69,3 @@ int main()
 		}
 	}
 }
-
-
-
-
-
-/*int main() {
-	int nx = 640;
-	int ny = 360;
-	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-
-	for (int j = ny - 1; j >= 0; j--) 
-	{
-		for (int i = 0; i < nx; i++) 
-		{
-			Vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
-			int ir = int(255.99 * col[0]);
-			int ig = int(255.99 * col[1]);
-			int ib = int(255.99 * col[2]);
-			std::cout << ir << " " << ig << " " << ib << "\n";
-		}
-	}
-}*/
